72fb2a438bee2e8dbd858acdf48a20017ff78283
ian-r-rose <ian.r.rose@gmail.com>
2020-06-03 15:50:01 -0700

    Add polling for data freshness to main.py

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ and send email.
 
 Author: @hunterowens
 """
-
-import papermill as pm 
-import pandas as pd
 import datetime
-import civis
 import os
+import sys
+import time
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import boto3
 
-import os, sys
+import boto3
+import civis
+import pandas as pd
+import papermill as pm 
+
 sys.path.append(os.getcwd())
 # check dir
 if not os.path.exists('outputs'):
@@ -26,13 +28,26 @@ output_path = f'./outputs/{str(datetime.datetime.now().date())}-coronavirus-stat
 # check if notebook is ready to run
 
 
-# execute notebook 
-pm.execute_notebook(
-   '/app/notebooks/county-city-indicators.ipynb',
-   output_path,
-   parameters=dict(start_date="4/15/2020"),
-   cwd='/app/notebooks'
-)
+# Try executing the notebook. If it fails due to data being incomplete,
+# try again in an hour, for a maximum of ten hours.
+MAX_TRIES = 10
+RETRY = 60 * 60
+for i in range(MAX_TRIES):
+    try:
+        pm.execute_notebook(
+           '/app/notebooks/county-city-indicators.ipynb',
+           output_path,
+           cwd='/app/notebooks'
+        )
+        break
+    except pm.PapermillExecutionError as e:
+        if "Data incomplete" in e.evalue:
+            print(f"Data incomplete, trying again in {RETRY} seconds")
+            time.sleep(RETRY)
+        else:
+            raise e
+else:
+    raise RuntimeError(f"Unable to get fresh data after {MAX_TRIES} tries.")
 
 # shell out, run NB Convert 
 output_format = 'PDFviaHTML'
