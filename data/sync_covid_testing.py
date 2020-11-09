2783865d96eceeb107f18d1b297c579efb2ac602
tiffanychu90 <tiffany.chu@lacity.org>
2020-11-09 13:55:49 -0800

    Fix testing script

--- data/sync_covid_testing.py
+++ data/sync_covid_testing.py
@@ -9,158 +9,6 @@ import pytz
 bucket_name = "public-health-dashboard"
 S3_FILE_PATH = f"s3://{bucket_name}/jhu_covid19/"
 
-filename = "COVID_testing_data.csv"
-workbook = (
-    "https://docs.google.com/spreadsheets/d/"
-    "1agPpAJ5VNqpY50u9RhcPOu7P54AS0NUZhvA2Elmp2m4/"
-    "export?format=xlsx&#gid=0"
-)
-sheet_name = "DUPLICATE OF MOPS"
-
-
-def get_county_data(filename, workbook, sheet_name):
-    df = pd.read_excel(workbook, sheet_name=sheet_name, skiprows=1, index_col=0)
-    df = df.T
-
-    select_rows = [1, 2, 3, 
-                7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
-                25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46 
-                ]
-    df = df.iloc[:, select_rows].reset_index(level=0)
-
-    column_names = [
-        "Date",
-        "City_Subtotal",
-        "County_Subtotal",
-        "Total_Performed",
-        "Dodgers_Stadium",
-        "Hansen_Dam",
-        "Crenshaw_Christian",
-        "Lincoln_Park",
-        "West_Valley_Warner",
-        "Carbon_Health_Echo_Park",
-        "Kedren_Health",
-        "Special_LAPD_LAFD",
-        "HACLA",
-        "Nursing_Home_Outreach",
-        "Homeless_Outreach",
-        "Hotchkin_Elysian_Park",
-        "VA_Lot15_Jackie_Robinson",
-        "Baldwin_Hills_Crenshaw",
-        "Northridge_Fashion",
-        "Pomona_Fairplex",
-        "Redondo_Beach_District",
-        "Palmdale",
-        "Long_Beach_CC",
-        "Charles_Drew_Campus",
-        "Santa_Clarita",
-        "East_LA_Civic",
-        "The_Forum",
-        "Bellflower_Civic",
-        "San_Gabriel_Valley_Airport",
-        "High_Desert_Lancaster",
-        "Northridge_Hospital_Medical",
-        "AltaMed",
-        "Cedars_Sinai",
-        "City_of_Bell",
-        "Glendale_Memorial",
-        "Beverly_Hospital_Montebello",
-        "Whittier_PIH",
-        "Good_Samaritan_LA",
-        "Harbor_UCLA_Medical",
-        "AVORS_Medical_Lancaster",
-        "Pasadena_Rose_Bowl",
-    ]
-
-    df.columns = column_names
-
-    # Drop the Total column
-    df = df[df.Date != "Total"]
-
-    df = (df.assign(
-        Date = pd.to_datetime(df.Date),
-        ).sort_values("Date")
-        .reset_index(drop=True)
-    )
-
-    # Fill in missing values with 0's for city sites
-    city_sites = [
-        "Dodgers_Stadium",
-        "Hansen_Dam",
-        "Crenshaw_Christian",
-        "Lincoln_Park",
-        "West_Valley_Warner",
-        "Carbon_Health_Echo_Park",
-        "Kedren_Health",
-        "Special_LAPD_LAFD",
-        "HACLA",
-        "Nursing_Home_Outreach",
-        "Homeless_Outreach",
-        "Hotchkin_Elysian_Park",
-        "VA_Lot15_Jackie_Robinson",
-        "Baldwin_Hills_Crenshaw",
-        "Northridge_Fashion",
-    ]
-
-    county_sites = [
-        "Pomona_Fairplex",
-        "Redondo_Beach_District",
-        "Palmdale",
-        "Long_Beach_CC",
-        "Charles_Drew_Campus",
-        "Santa_Clarita",
-        "East_LA_Civic",
-        "The_Forum",
-        "Bellflower_Civic",
-        "San_Gabriel_Valley_Airport",
-        "High_Desert_Lancaster",
-        "Northridge_Hospital_Medical",
-        "AltaMed",
-        "Cedars_Sinai",
-        "City_of_Bell",
-        "Glendale_Memorial",
-        "Beverly_Hospital_Montebello",
-        "Whittier_PIH",
-        "Good_Samaritan_LA",
-        "Harbor_UCLA_Medical",
-        "AVORS_Medical_Lancaster",
-        "Pasadena_Rose_Bowl",
-    ]
-
-    # Sometimes a TBD or unknown is found. Coerce to be NaNs until it is filled in later.
-    replace_me = ["TBD", "tbd", "unknown", "Unknown", "UNKNOWN", "unk"]
-
-    def replace_str_with_zero(df, cols_to_apply):
-        df[cols_to_apply] = df[cols_to_apply].replace(replace_me, 0)
-        df[cols_to_apply] = df[cols_to_apply].fillna(0).astype(int)
-        
-        return df
-    
-    total_cols = ["City_Subtotal", "County_Subtotal", "Total_Performed"]
-
-    df = replace_str_with_zero(df, city_sites)
-    df = replace_str_with_zero(df, county_sites)
-    df = replace_str_with_zero(df, total_cols)
-
-    df = (df.assign(
-            City_Subtotal=df.City_Subtotal.astype(int),
-            County_Subtotal=df.County_Subtotal.astype(int),
-            Total_Performed=df.Total_Performed.astype(int),
-        ).rename(columns = {
-            "City_Subtotal":"City_Performed",
-            "County_Subtotal": "OtherCounty_Performed",
-            "Total_Performed": "Performed"
-        })
-    )
-    
-    df = (df.sort_values("Date")
-            .reset_index(drop=True)
-    )
-
-    df.to_parquet(f"{S3_FILE_PATH}county-city-testing-sites.parquet")
-    
-    return df
-
         
 """
 Mayor's office no longer updating county test totals.
@@ -181,7 +29,7 @@ def update_persons_tested_rshiny():
     df = (df.assign(
             date = pd.to_datetime(df.date_use),
         ).rename(columns = {"persons_tested_all": "persons_tested",
-                           "positive_persons_all": "persons_pos"})
+                           "positive_persons": "persons_pos"})
           .sort_values("date")
           .reset_index(drop=True)
         [keep]  
@@ -217,17 +65,9 @@ don't want to lose the distinction between city/county sites
 and totals including private healthcare facilities.
 """
 def update_covid_testing_city_county_data(**kwargs):
-    city_county_df = get_county_data(filename, workbook, sheet_name)
     persons_tested_df = update_persons_tested_rshiny()
     tests_performed_df = update_tests_performed_rshiny()
 
-    # Clean up columns before merging
-    keep = ["Date", "City_Performed", "Performed"]
-    city_county_df = (city_county_df[keep]
-                      .rename(columns = {"Date": "date",
-                                         "City_Performed": "City_Site_Performed", 
-                                         "Performed": "County_Site_Performed"})
-                     )
 
     # County has the number of positive for each test batch
     persons_tested_df = persons_tested_df.rename(
@@ -240,15 +80,13 @@ def update_covid_testing_city_county_data(**kwargs):
     
     # Merge together
     # County's data goes back to 3/10, earlier than Mayor's spreadsheet, which is 3/20
-    df1 = pd.merge(persons_tested_df, tests_performed_df, 
+    df = pd.merge(persons_tested_df, tests_performed_df, 
                     on = "date", how = "left", validate = "1:1")
 
-    df = pd.merge(df1, city_county_df, 
-                  on = "date", how = "left", validate = "1:1")
     
     integrify_me = ["County_Person_Performed", "County_Person_Positive",
-                    "County_Performed", "County_Positive", 
-                    "City_Site_Performed", "County_Site_Performed"]
+                    "County_Performed", "County_Positive",
+                   ]
     
     df[integrify_me] = df[integrify_me].astype("Int64")
     
