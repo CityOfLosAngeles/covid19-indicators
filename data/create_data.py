d362b8a16bb54ca2122137feeeec3c11105f2fde
tiffanychu90 <tiffany.chu@lacity.org>
2021-02-02 08:36:51 -0800

    Change schedule frequency

--- data/create_data.py
+++ data/create_data.py
@@ -5,9 +5,5 @@ Less frequent updates.
 import jhu
 import jhu_county
 
-import la_neighborhood
-
 jhu.load_global_covid_data()
 jhu_county.append_county_time_series()
-
-la_neighborhood.update_neighborhood_data()
--- data/create_data_AM_hourly.py
+++ data/create_data_AM_hourly.py
@@ -4,10 +4,12 @@ Hourly updates because these depend on Google sheets.
 """
 import sync_covid_testing
 import sync_la_cases
-
 import ca_hospital
+import la_neighborhood
 
 sync_covid_testing.update_covid_testing_city_county_data()
 sync_la_cases.update_la_cases_data()
 
 ca_hospital.update_ca_surge_hospital_data()
+
+la_neighborhood.update_neighborhood_data()
