a246d6b49f8e6e45f122e410968d4c47a3cc8e5c
Angel <darkpaper@hotmail.com>
2021-05-13 17:14:14 -0700

    try relative python path

--- invoke.go
+++ invoke.go
@@ -25,18 +25,12 @@ func main() {
 }
 
 func scriptHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("step1")
-	cmd := exec.CommandContext(r.Context(), "/opt/conda/bin/python", "test1.py")
-	log.Printf("step2")
+	cmd := exec.CommandContext(r.Context(), "python", "test1.py")
 	cmd.Stderr = os.Stderr
-	log.Printf("step3")
 	out, err := cmd.Output()
-	log.Printf("step4")
 	if err != nil {
-		log.Printf("step5")
 		w.WriteHeader(500)
 	}
-	log.Printf("step6")
 	w.Write(out)
 }
 
