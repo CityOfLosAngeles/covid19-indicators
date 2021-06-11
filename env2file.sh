a8e569aa8983fcf7503e8b7cc1d468b31e7145dd
tiffanychu90 <tiffany.chu@lacity.org>
2021-06-11 13:59:14 -0700

    Incorporate changes for Cloud Build/Run

---
+++ env2file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+FNAME="$2"
+ENAME="$1"
+CMD="echo \$$ENAME"
+if [[ y`eval $CMD` = 'y' ]]; then
+	echo ERROR: Environment variable $ENAME is empty
+	exit
+fi
+CMD="echo \$$ENAME |sed -e 's/[\]n/\n/g' > $FNAME"
+echo writing $ENAME to  $FNAME
+eval $CMD
