7a98e81b72aabe5da83beeed440be2ee5a099145
tiffanychu90 <tiffany.chu@lacity.org>
2021-09-02 14:32:53 -0700

    Add socrata_utils

--- processing_utils/__init__.py
+++ processing_utils/__init__.py
@@ -3,11 +3,13 @@ from . import utils, us_county_utils
 from . import make_charts, make_maps, neighborhood_charts
 from . import meet_indicators, ca_reopening_tiers
 from . import neighborhood_utils
+from . import socrata_utils
 
 __version__ = "0.1.0"
 
 __all__ = ["default_parameters", "useful_dict", 
            "utils", "us_county_utils",
            "make_charts", "make_maps", "neighborhood_charts",
-           "meet_indicators", "ca_reopening_tiers", "neighborhood_utils"]
+           "meet_indicators", "ca_reopening_tiers", "neighborhood_utils", 
+           "socrata_utils"]
 
