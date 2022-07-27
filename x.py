44b4da98a3bdc3cc1a2f1a2ae31d09c2d6fdfb81
Angel Arafiles <74928824+angelarafiles-lacityorg@users.noreply.github.com>
2022-07-27 11:09:09 -0700

    remove options

--- x.py
+++ x.py
@@ -64,11 +64,11 @@ def run(argv=None, save_main_session=True):
 
   # We use the save_main_session option because one or more DoFn's in this
   # workflow rely on global context (e.g., a module imported at module level).
-  pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+  #pipeline_options = PipelineOptions(pipeline_args)
+  #pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
 
   # The pipeline will be run on exiting the with block.
-  with beam.Pipeline(options=pipeline_options) as p:
+  with beam.Pipeline() as p:
 
     # Read the text file[pattern] into a PCollection.
     lines = p | 'Read' >> ReadFromText('/app/rjl')
