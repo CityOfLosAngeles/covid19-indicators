56c13dcd706eeb55ac0585cc028284249a8d941e
Angel Arafiles <74928824+angelarafiles-lacityorg@users.noreply.github.com>
2022-10-02 23:39:08 -0700

    debug

--- dataflow_start.py
+++ dataflow_start.py
@@ -36,7 +36,8 @@ class GenRunScript(beam.DoFn):
         logging.info("in GenRunscdrit")
         logging.info(str(type(args_str_element)))
         logging.info("args_str_element="+str(args_str_element))
-        env=json.loads(args_str_element[1])
+        logging.info("args_str_element[1]="+str(args_str_element[1]))
+        env=json.loads(str(args_str_element[1]))
         env_list=env[env]
         f=open('temp_shell.sh','w')
         print("#!/bin/bash",file=f)
